ea62389f1eaa3e29cd40fb19190be07c51dd7fd7
57Anonymous34 <A.Jashari.inf24@stud.bbbaden.ch>
2026-01-16 11:38:37 +0100

    Add files via upload

--- main.lua
+++ main.lua
@@ -1,10 +1,61 @@
-print("Hallo! Lua funktioniert !")
+
+local enemy = require "Enemy"
+local game = {
+   difficulty = 1,
+   state = {
+      menu = false,
+      paused = false,
+      running = true,
+      ended = false,
+   }
+}
+
+local player = {
+   radius = 20,
+   x = 30,
+   y = 30,
+}
+
+local enemies = {}
+
+function love.load()
+   love.window.setTitle("Save the ball")
+   love.mouse.setVisible(false)
 
 
+   table.insert(enemies, 1, enemy())
+end
 
-x = 5
-print(x)
+
+function love.update()
+   player.x, player.y = love.mouse.getPosition()
+
+   for i = 1, #enemies do
+      enemies[i]:move(player.x, player.y)
+   end
+end
 
 
-a = "Mein Name ist Amar und ich bin ready für eine neue Programmiersprache"
-print(a)
+function love.draw()
+   love.graphics.printf(
+   "FPS: " .. love.timer.getFPS(), 
+   love.graphics.newFont(16), 
+   10, 
+   love.graphics.getHeight() - 30, 
+   love.graphics.getWidth()
+)
+   
+   if game.state ["running"] then
+      for i = 1, #enemies do
+      enemies[i]:draw()
+   end
+
+      love.graphics.circle("fill", player.x, player.y, player.radius / 2)
+   end
+
+
+   if game.state ["running"] then
+      love.graphics.circle("fill", player.x, player.y, player.radius)
+   end
+
+end
